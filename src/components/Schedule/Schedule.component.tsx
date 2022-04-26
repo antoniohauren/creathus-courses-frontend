4c5b9ee2070af81a90577dcdda074b02c81d2151
Antonio Hauren <antoniohauren@gmail.com>
2022-04-25 21:19:35 -0300

    refactor: move svgs to it own files on public folder

--- src/components/Schedule/Schedule.component.tsx
+++ src/components/Schedule/Schedule.component.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Schedule.module.css";
+import { ReactComponent as CalendarIcon } from "../../../public/svgs/calendar.svg";
 
 interface ScheduleProps {
   timeDate: string;
@@ -28,20 +29,7 @@ const Schedule = ({ timeDate, variant, ...props }: ScheduleProps) => {
 
   return (
     <div className={styles.schedule}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth={2}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-        />
-      </svg>
+      <CalendarIcon />
       <div className={styles.dateText}>
         {variant === "start" ? <p>inicio em</p> : <p>término</p>}
         <p data-testid="dateString">{dateString}</p>
